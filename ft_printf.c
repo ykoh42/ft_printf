b19eba847d16b755608f7ee5800f48a25ab6778b
ykoh <ykoh@student.42seoul.kr>
2020-05-30 23:42:42 +0900

    Add put_conversion

--- ft_printf.c
+++ ft_printf.c
@@ -5,23 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/11 14:52:43 by ykoh              #+#    #+#             */
-/*   Updated: 2020/06/11 15:05:25 by ykoh             ###   ########.fr       */
+/*   Created: 2020/06/18 15:31:14 by ykoh              #+#    #+#             */
+/*   Updated: 2020/06/21 14:26:24 by ykoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char * restrict format, ...)
+int	ft_printf(const char *restrict format, ...)
 {
-	va_list	ap;
-	int		cnt;
+	va_list		ap;
+	long long	cnt;
 
+	cnt = 0;
 	va_start(ap, format);
-
-
-
-
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			cnt += write(1, format, 1);
+		}
+		else
+		{
+			if (put_conversion(&format, ap, &cnt) < 0)
+				return (-1);
+		}
+		format++;
+	}
 	va_end(ap);
-	return (cnt);
+	return ((int)cnt);
 }
