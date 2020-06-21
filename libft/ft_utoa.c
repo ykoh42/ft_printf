8663e0033498739b2abf558a836ae4b44fbedee4
ykoh <ykoh@student.42seoul.kr>
2020-06-21 23:42:42 +0900

    Add ft_utoa.c

---
+++ libft/ft_utoa.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/22 17:01:08 by ykoh              #+#    #+#             */
+/*   Updated: 2020/06/25 13:54:49 by ykoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static	size_t	ft_numlen(unsigned long long n)
+{
+	size_t	len;
+
+	len = 0;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char			*ft_utoa(unsigned long long n)
+{
+	char			*num;
+	int				rem;
+	size_t			i;
+	const size_t	num_len = ft_numlen(n);
+
+	if (!(num = ft_calloc(num_len + 1, sizeof(char))))
+		return (NULL);
+	i = 0;
+	while (i < num_len)
+	{
+		rem = n % 10;
+		n = n / 10;
+		num[i] = "0123456789"[rem];	
+		i++;
+	}
+	return (ft_strrev(num));
+}
