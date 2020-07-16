67dba72e0bc5ab1b34d187982fd8026368734b8a
ykoh <ykoh@student.42seoul.kr>
2020-07-16 14:50:30 +0900

    Fix convention

--- libft/ft_dtoa_3.c
+++ libft/ft_dtoa_3.c
@@ -11,6 +11,21 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+static char	*reset_ret(const char *p, const size_t m)
+{
+	int				exp;
+	char			*tmp[2];
+
+	exp = ft_atoi(ft_strchr(p, 'e') + 1);
+	exp += m;
+	tmp[0] = ft_strndup(p, ft_strchr(p, 'e') - p);
+	tmp[1] = (ft_abs(exp) > 9) ? ft_itoa(ft_abs(exp)) :
+		ft_strjoin_free(ft_strdup("0"), ft_itoa(ft_abs(exp)));
+	tmp[1] = (exp >= 0) ? ft_strjoin_free(ft_strdup("e+"), tmp[1]) :
+						ft_strjoin_free(ft_strdup("e-"), tmp[1]);
+	free((char *)p);
+	return (ft_strjoin_free(tmp[0], tmp[1]));
+}
 
 static char	*has_point(char *ret, int precision, char specifier)
 {
@@ -41,8 +56,6 @@ char	*e_correction(char *ret, int precision, char specifier)
 	const char		*p = ret;
 	const char		*e = ft_strchr(p, 'e');
 	const size_t	m = (size_t)ft_log10(ft_abs(ft_atoi(p)));
-	char			*tmp[2];
-	int				exp;
 
 	if (ft_abs(ft_atoi(p)) >= 10)
 	{
@@ -59,15 +72,7 @@ char	*e_correction(char *ret, int precision, char specifier)
 			ft_strlcpy(ft_strchr(p, '.') + precision + 1, e, ft_strlen(e) + 2);
 		else
 			ft_strlcpy(ret - m, e, ft_strlen(e) + 2);
-		exp = ft_atoi(ft_strchr(p, 'e') + 1);
-		exp += m;
-		tmp[0] = ft_strndup(p, ft_strchr(p, 'e') - p);
-		tmp[1] = (ft_abs(exp) > 9) ? ft_itoa(ft_abs(exp)) :
-			ft_strjoin_free(ft_strdup("0"), ft_itoa(ft_abs(exp)));
-		tmp[1] = (exp >= 0) ? ft_strjoin_free(ft_strdup("e+"), tmp[1]) :
-							ft_strjoin_free(ft_strdup("e-"), tmp[1]);
-		free((char *)p);
-		ret = ft_strjoin_free(tmp[0], tmp[1]);
+		ret = reset_ret(p, m);
 	}
 	else if (ft_strchr(p, '.'))
 		ret = has_point(ret, precision, specifier);
